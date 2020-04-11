caee9ec714649cd85e99a7038129e3438ff78922
Carlo Nicora <carlo.nicora@gmail.com>
2020-04-11 21:22:37 +0100

    activeCampaign wrapper updated

--- src/activeCampaign.php
+++ src/activeCampaign.php
@@ -13,14 +13,18 @@ use carlonicora\minimalism\services\MySQL\exceptions\dbUpdateException;
 use carlonicora\minimalism\services\MySQL\MySQL;
 use Exception;
 use Mediatoolkit\ActiveCampaign\Client;
+use Mediatoolkit\ActiveCampaign\Tags\Tags;
 use RuntimeException;
 
 class activeCampaign extends abstractService {
     /** @var activeCampaignConfigurations  */
     private activeCampaignConfigurations $configData;
 
-    /** @var contacts  */
-    private contacts $contacts;
+    /** @var contacts|null  */
+    private ?contacts $contacts=null;
+
+    /** @var Client|null  */
+    private ?Client $client=null;
 
     /**
      * activeCampaign constructor.
@@ -56,35 +60,62 @@ class activeCampaign extends abstractService {
     /**
      * @return Client
      */
-    private function client(): Client {
-        return new Client($this->configData->url, $this->configData->key);
+    private function activeCampaignClient(): Client {
+        if ($this->client === null) {
+            $this->client = new Client($this->configData->url, $this->configData->key);
+        }
+
+        return $this->client;
+    }
+
+    /**
+     * @return \Mediatoolkit\ActiveCampaign\Contacts\Contacts
+     */
+    private function activeCampaignContacts() : \Mediatoolkit\ActiveCampaign\Contacts\Contacts {
+        return new \Mediatoolkit\ActiveCampaign\Contacts\Contacts($this->activeCampaignClient());
+    }
+
+    /**
+     * @return Tags
+     */
+    private function activeCampaignTags() : Tags {
+        return new Tags($this->activeCampaignClient());
     }
 
     /**
      * @param int $userId
-     * @param string $unsubscribeLink
      * @param string $email
+     * @param string $unsubscribeLink
      * @return void
      * @throws dbSqlException
      * @throws dbUpdateException
      * @throws Exception
      */
-    public function subscribe(int $userId, string $unsubscribeLink, string $email) : void {
-        /*
-        $activeContact = array(
-            'email' => $email,
-            'field[' .$this->configData->unsubscribeField.',0]' => $unsubscribeLink,
-            "p[{$this->configData->listId}]" => $this->configData->listId,
-            "status[{$this->configData->listId}]" => 1
-        );
+    public function subscribe(int $userId, string $email, string $unsubscribeLink) : void {
+        $activeCampaignContacts = $this->activeCampaignContacts();
 
-        $apiResponse = $this->sendRequest('contact_sync', $activeContact);
+        try {
+            $retrievedContactJson = $activeCampaignContacts->sync([
+                'email' => $email
+            ]);
 
-        if ($apiResponse['result_code'] === 0) {
-            throw new RuntimeException($apiResponse['result_message']);
+            $retrievedContact = json_decode($retrievedContactJson, true, 512, JSON_THROW_ON_ERROR);
+            $contactId = $retrievedContact['contact']['id'];
+
+            $activeCampaignContacts->updateListStatus([
+                'list' => $this->configData->listId,
+                'contact' => $contactId,
+                'status' => 1
+            ]);
+
+            $activeCampaignContacts->createCustomFieldValue(
+                $contactId,
+                $this->configData->unsubscribeField,
+                $unsubscribeLink
+            );
+        } catch (Exception $e) {
+            throw new RuntimeException('Error contacting the mail service', $e->getCode());
         }
-
-        $contactId = (int)$apiResponse['subscriber_id'];
 
         $contact = [
             'userId' => $userId,
@@ -92,35 +123,26 @@ class activeCampaign extends abstractService {
         ];
 
         $this->contacts->update($contact);
-        */
     }
 
     /**
      * @param int $userId
-     * @param string $email
      * @throws Exception
      */
-    public function unsubscribe(int $userId, string $email) : void {
+    public function unsubscribe(int $userId) : void {
         $contact = $this->contacts()->userId($userId);
 
-        $contacts = new \Mediatoolkit\ActiveCampaign\Contacts\Contacts($this->client());
-        $response = $contacts->updateListStatus(['list' => 2, 'contact' => $contact['contactId'], 'status' => 2]);
+        $activeCampaignContacts = $this->activeCampaignContacts();
 
-
-        /*
-        $activeContact = array(
-            'id' => $contact['contactId'],
-            "p[{$this->configData->listId}]" => $this->configData->listId,
-            "status[{$this->configData->listId}]" => 2,
-            'overwrite' => 0
-        );
-
-        $apiResponse = $this->sendRequest('contact_edit', $activeContact);
-
-        if ($apiResponse['result_code'] === 0) {
-            throw new RuntimeException($apiResponse['result_message']);
+        try {
+            $activeCampaignContacts->updateListStatus([
+                'list' => $this->configData->listId,
+                'contact' => $contact['contactId'],
+                'status' => 2
+            ]);
+        } catch (Exception $e) {
+            throw new RuntimeException('Error contacting the mail service', $e->getCode());
         }
-        */
     }
 
     /**
@@ -129,66 +151,30 @@ class activeCampaign extends abstractService {
      * @throws Exception
      */
     public function addTag(int $userId, string $tag) : void{
-        /*
         $contact = $this->contacts()->userId($userId);
 
-        $activeContact = array(
-            'id' => $contact['activeCampaignId'],
-            'tag' => $tag
-        );
+        $activeCampaignTags = $this->activeCampaignTags();
+        $jsonTags = $activeCampaignTags->listAll([$tag]);
+        $tagsArray = json_decode($jsonTags, true, 512, JSON_THROW_ON_ERROR);
 
-        $apiResponse = $this->sendRequest('contact_tag_add', $activeContact);
-
-        if ($apiResponse['result_code'] === 0) {
-            throw new RuntimeException($apiResponse['result_message']);
-        }
-        */
-    }
-
-    /**
-     * @param string $action
-     * @param array $post
-     * @return array
-     * @throws Exception
-     */
-    private function sendRequest(string $action, array $post) : array {
-        $params = [
-            'api_key' => $this->configData->key,
-            'api_action'   => $action,
-            'api_output'   => 'serialize'
-        ];
-
-        $query = '';
-        foreach( $params as $key => $value ) {
-            $query .= urlencode($key) . '=' . urlencode($value) . '&';
-        }
-        $query = rtrim($query, '& ');
-
-        $data = '';
-        foreach( $post as $key => $value ) {
-            $data .= urlencode($key) . '=' . urlencode($value) . '&';
-        }
-        $data = rtrim($data, '& ');
-
-        $url = rtrim($this->configData->url, '/ ');
-
-        $api = $url . '/admin/api.php?' . $query;
-
-        $request = curl_init($api);
-        curl_setopt($request, CURLOPT_HEADER, 0);
-        curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($request, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($request, CURLOPT_FOLLOWLOCATION, true);
-
-        $apiResponse = (string)curl_exec($request);
-
-        curl_close($request); // close curl object
-
-        if ( !$apiResponse ) {
-            throw new RuntimeException('Error receiveing data', 500);
+        $tagId = null;
+        foreach ($tagsArray['tags'] as $tagArray){
+            if ($tagArray['tag'] === $tag){
+                $tagId = $tagArray['id'];
+                break;
+            }
         }
 
-        /** @noinspection UnserializeExploitsInspection */
-        return unserialize($apiResponse);
+        if ($tagId === null) {
+            throw new RuntimeException('Tag not found', 404);
+        }
+
+        $activeCampaignContacts = $this->activeCampaignContacts();
+
+        try {
+            $activeCampaignContacts->tag($contact['contactId'], $tagId);
+        } catch (Exception $e) {
+            throw new RuntimeException('Error contacting the mail service', $e->getCode());
+        }
     }
 }
